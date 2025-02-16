3a7e724e7d6caa43846a46e5de2933487bed086e
Kenza Houben <kenza.tabti@student.ehb.be>
2025-02-16 17:42:53 +0100

    warmup 1a klaar, maar 2 problemen

--- main.js
+++ main.js
@@ -5,9 +5,25 @@ import {
 } from './lib.js';
 
 runTitle();
+warmup1();
 
 function warmup1() {
     console.log("Exercise week 1");
+
+    // Oefening: plaats je naam
+
+    const p = "Hallo, dit is de oefening van Kenza";
+
+    document.getElementById("button-1a").addEventListener('click', function () {
+        document.getElementById("content-1").innerHTML += p;
+    });
+
+    // Oefening: verander de achtergrond
+
+    document.getElementById("button-1b").addEventListener('click', function () {
+
+    })
+
 }
 
 function warmup2() {
